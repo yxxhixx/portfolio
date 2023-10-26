4f0f922733ae99b0b6b2a374e29157535bf2205b
yxxhixx <149113629+yxxhixx@users.noreply.github.com>
2023-10-26 23:17:53 +0200

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,42 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+
+    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $body = $_POST['body'];
+
+        require_once "PHPMailer/PHPMailer.php";
+        require_once "PHPMailer/SMTP.php";
+        require_once "PHPMailer/Exception.php";
+
+        $mail = new PHPMailer();
+
+        //SMTP Settings
+        $mail->isSMTP();
+        $mail->Host = "";
+        $mail->SMTPAuth = true;
+        $mail->Username = ""; //enter your email address username
+        $mail->Password = ''; //enter you email password
+        $mail->Port = 465;
+        $mail->SMTPSecure = "ssl";
+
+        //Email Settings
+        $mail->isHTML(true);
+        $mail->setFrom($email, $name);
+        $mail->addAddress(""); //enter you email address 
+        $mail->Subject = ("$email ($subject)");
+        $mail->Body = $body;
+
+        if ($mail->send()) {
+            $status = "success";
+            $response = "Email is sent!";
+        } else {
+            $status = "failed";
+            $response = "Something is wrong: <br><br>" . $mail->ErrorInfo;
+        }
+
+        exit(json_encode(array("status" => $status, "response" => $response)));
+    
+?>
